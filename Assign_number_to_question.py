e46829cd85b6f52d1a469cbee541189ce4d6c866
黃文龍 <wlhunag@gmail.com>
2013-12-30 21:58:00 +0800

    Add answer styleing

--- Assign_number_to_question.py
+++ Assign_number_to_question.py
@@ -12,20 +12,22 @@ class MW(QWidget,Ui_Form):
         super(MW,self).__init__(parent)
         self.setupUi(self)
         self.create_connection()
-        self.var = ['A','<A>','[A]','{A}']
+        self.var = ['ABCD','<A><B><C><D>','[A][B][C][D]','{A}{B}{C}{D}']
         self.comboBox.addItems(self.var)
         self.show()
 
     def create_connection(self):
         self.plainTextEdit.textChanged.connect(self.manipulation)
+        self.comboBox.currentIndexChanged.connect(lambda: self.manipulation())
 
     def manipulation(self):
+
         self.ori = unicode(self.plainTextEdit.toPlainText()).encode('utf-8').lstrip("\n").rstrip('\n')
         poll = ['','A','B','C','D']
         print self.ori
-
-        for a,b in zip(poll,['\t{0}'.format(answer) for answer in self.ori.split('\n')]):
-            result = "\n".join(a + '.' + b)
+        result = ''
+        for a,b in zip(poll,['{0}'.format(answer) for answer in self.ori.split('\n')]):
+            result += a + '.' + b + "\n"
         print result
         self.textBrowser.setText(result.decode('utf-8'))
 
