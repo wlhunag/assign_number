409484f7109bb8c60730ad4de47af18694d68c77
黃文龍 <wlhunag@gmail.com>
2013-12-31 17:17:19 +0800

    add icon image and package

---
+++ setup_cxfreeze.py
@@ -0,0 +1,22 @@
+#-*- coding: utf-8-*-
+__author__ = 'Aaron'
+import sys
+
+from cx_Freeze import setup, Executable
+
+
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+#因為已經包含下列檔案了，所以就comment掉了
+includefiles = [ 'playtext.pyc',"exam.ico"]
+#記得要加上C:\Python27\Lib\site-packages\PyQt4\plugins\imageformats 這個資料夾
+includes = ['sip', 'PyQt4.QtCore','PyQt4.QtGui','atexit']
+
+
+setup(
+        name = "Assign-number",
+        version = "0.5",
+        description = u"加上答案標號",
+        options = {'build_exe': {'include_files':includefiles,"includes": includes}},
+        executables = [Executable("Assign_number_to_question.py" ,base = base, icon = "exam.ico")])
